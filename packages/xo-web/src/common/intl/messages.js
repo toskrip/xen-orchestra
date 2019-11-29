d813dde156a6a20f89acc17b2c3aa63f2d6f398a
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2019-11-29 13:54:02 +0100

    feat(xo-web/sr/disks): ability to migrate VDI
    fix #4455

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -2088,6 +2088,7 @@ const messages = {
   srAllDisconnected: 'Disconnected',
   srSomeConnected: 'Partially connected',
   srAllConnected: 'Connected',
+  migrateSelectedVdis: 'Migrate selected VDIs',
 
   // ----- XOSAN -----
   xosanTitle: 'XOSAN',
